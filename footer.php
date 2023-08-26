2a1ba61d4a6485ea21fc210720e03af1baa4ba4a
Pranitapagare <126388613+Pranitapagare@users.noreply.github.com>
2023-08-26 16:54:16 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+	<li class="about">
+					<p>Rental Fleet is an organized team that rents cars and other vehicles to clients at lower costs. We provide Professional and well educated Drivers.</p>
+				</li>
+			</ul>
+		</div>
+
+		<div class="copyrights wrapper">
+			Copyright &copy; <?php echo date("Y")?> All Rights Reserved.
+		</div>
+	</footer><!--  end footer  -->
+	
+</body>
+</html>
